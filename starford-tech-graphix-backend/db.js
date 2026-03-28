ffde9c9ba7e1f8f897925c26fa2ed148ae225476
tech-starford <tech.starford@gmail.com>
2026-03-28 18:51:54 +0300

    ne changes in backend

--- starford-tech-graphix-backend/db.js
+++ starford-tech-graphix-backend/db.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/* const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+}); */
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }   // required for Render
 });
 
 module.exports = {
